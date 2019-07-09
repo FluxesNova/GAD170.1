aa2446ff8ee9aa7f09cd50146ad5d77d64e6875a
Anany Agarwal <anany2505@gmail.com>
2019-07-09 20:41:42 +0400

    Made the XP system for the dancce game

---
+++ Assets/Scripts/StatsGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  A <see langword="static"/> class with methods (functions) for initialising or randomising the stats class.
+///  
+/// TODO:
+///     Initialise a stats instance with generated starting stats
+///     Handle the assignment of extra points or xp into an existing stats of a character
+///         - this is expected to be used by NPCs leveling up to match the player.
+/// </summary>
+public static class StatsGenerator
+{
+    public static void InitialStats(Stats stats)
+    {
+        stats.xp = 1;
+        stats.level = 1;
+        stats.luck = Random.Range(1, 100 + 1);
+        stats.rhythm = 5;
+        stats.style = 3;
+        stats.totalstats = stats.level + stats.luck + stats.rhythm + stats.style;
+        stats.xpneeded = stats.level * 100;
+    }
+
+    public static void AssignUnusedPoints(Stats stats, int points)
+    {
+    }
+}
---
+++ Assets/Scripts/XPHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for converting a battle result into xp to be awarded to the player.
+/// 
+/// TODO:
+///     Respond to battle outcome with xp calculation based on;
+///         player win 
+///         how strong the win was
+///         stats/levels of the dancers involved
+///     Award the calculated XP to the player stats
+///     Raise the player level up event if needed
+/// </summary>
+public class XPHandler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameEvents.OnBattleConclude += GameEvents_OnBattleConclude;
+    }
+
+    private void GameEvents_OnBattleConclude(BattleResultEventData data)
+    {
+        GainXP(data);
+
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    public void GainXP(BattleResultEventData data)
+    {
+        print("works");
+        data.player.xp = (int)((data.npc.level / data.player.level) * ((data.player.rhythm / data.player.style) * 10) + data.outcome * 100);
+
+        if (data.player.xp >= data.player.xpneeded)
+        {
+            data.player.level++;
+            data.player.xp = 0;
+            data.player.rhythm = data.player.level * 5;
+            data.player.style = data.player.level * 3;
+        }
+    }
+}
